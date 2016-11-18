be16039b73faf304b5569e7a1ddbba3d59a900a3
Maxime Tricoire <max.tricoire@gmail.com>
2016-11-18 17:08:41 +0100

    prevent worker from linting kevscript on every changes

--- build/src-noconflict/worker-kevscript.js
+++ build/src-noconflict/worker-kevscript.js
@@ -1434,18 +1434,16 @@ ace.define("ace/mode/kevscript_worker",["require","exports","module","ace/lib/oo
   function KevScriptWorker(sender) {
     Mirror.call(this, sender);
     this.delayedCompletions = [];
+    this.setTimeout(0);
+
     var logger = {
       info: function (tag, msg) {
-        sender.emit('log', { type: 'info', tag: tag, message: msg });
       },
       debug: function (tag, msg) {
-        sender.emit('log', { type: 'debug', tag: tag, message: msg });
       },
       warn: function (tag, msg) {
-        sender.emit('log', { type: 'warn', tag: tag, message: msg });
       },
       error: function (tag, msg) {
-        sender.emit('log', { type: 'error', tag: tag, message: msg });
       },
       setLevel: function() {},
       setFilter: function() {}
@@ -1472,47 +1470,53 @@ ace.define("ace/mode/kevscript_worker",["require","exports","module","ace/lib/oo
   oop.inherits(KevScriptWorker, Mirror);
 
   (function() {
+    this.lastTimeout = null;
+
     this.onUpdate = function () {
-      if (this.kevs) {
-        var script = this.doc.getValue();
-        if (!script) {
-          this.sender.emit('lint', []);
-        } else {
-          this.kevs.parse(script, function(err, model, warnings) {
-            var errors = [];
-            if (err) {
-              if (err.nt) {
-                var message = 'Unable to match \'' + err.nt + '\'';
-                if (err.nt === 'ws') {
-                  message = 'Unable to match \'whitespace\'';
-                } else if (err.nt === 'kevScript') {
-                  message = 'A line must start with a statement (add, attach, set, etc.)';
-                } else if (TOKENS.indexOf(err.nt) >= 0) {
-                  message = 'Expected statement or comment (do you mean \'' + err.nt.split('Token').shift() + '\'?)';
-                }
-                var pos = this.doc.indexToPosition(err.pos[0], 0);
-                errors.push({
-                  row: err.line - 1,
-                  column: err.col,
-                  text: message,
-                  type: 'error'
-                });
-              } else {
-                if (err && err.pos) {
+      clearTimeout(this.lastTimeout);
+      this.lastTimeout = setTimeout(function () {
+        if (this.kevs) {
+          var script = this.doc.getValue();
+          if (!script) {
+            this.sender.emit('lint', []);
+          } else {
+            this.sender.emit('lintStart');
+            this.kevs.parse(script, function(err, model, warnings) {
+              var errors = [];
+              if (err) {
+                if (err.nt) {
+                  var message = 'Unable to match \'' + err.nt + '\'';
+                  if (err.nt === 'ws') {
+                    message = 'Unable to match \'whitespace\'';
+                  } else if (err.nt === 'kevScript') {
+                    message = 'A line must start with a statement (add, attach, set, etc.)';
+                  } else if (TOKENS.indexOf(err.nt) >= 0) {
+                    message = 'Expected statement or comment (do you mean \'' + err.nt.split('Token').shift() + '\'?)';
+                  }
                   var pos = this.doc.indexToPosition(err.pos[0], 0);
                   errors.push({
-                    row: pos.row,
-                    column: pos.column,
-                    text: err.message,
+                    row: err.line - 1,
+                    column: err.col,
+                    text: message,
                     type: 'error'
                   });
+                } else {
+                  if (err && err.pos) {
+                    var pos = this.doc.indexToPosition(err.pos[0], 0);
+                    errors.push({
+                      row: pos.row,
+                      column: pos.column,
+                      text: err.message,
+                      type: 'error'
+                    });
+                  }
                 }
               }
-            }
-            this.sender.emit('lint', errors);
-          }.bind(this));
+              this.sender.emit('lint', errors);
+            }.bind(this));
+          }
         }
-      }
+      }.bind(this), 500);
     };
 
     this.getCompletions = function (token, pos, prefix, callbackId) {
